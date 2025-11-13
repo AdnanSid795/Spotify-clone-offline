3f8042d82b5db57ec78329e9d912f86e851ef417
AdnanSid795 <siddiquiadnan327@gmail.com>
2025-11-13 21:45:55 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -3,10 +3,23 @@ self.addEventListener("install", event => {
     caches.open("spotify-cache-v1").then(cache => {
       return cache.addAll([
         "./",
-        "./index.html",
-        "./manifest.json",
-        "./icon-192.png",
-        "./icon-512.png"
+        "/index.html",
+        "/manifest.json",
+        "/icon-192.png",
+        "/icon-512.png",
+
+        // --- MUSIC FILES ---
+        "/music/song0.mp3",
+        "/music/song1.mp3",
+        "/music/song2.mp3",
+        "/music/song3.mp3",
+        "/music/song4.mp3",
+        "/music/song5.mp3",
+        "/music/song6.mp3",
+        "/music/song7.mp3",
+        "/music/song8.mp3",
+        "/music/song9.mp3",
+        "/music/song10.mp3"
       ]);
     })
   );
