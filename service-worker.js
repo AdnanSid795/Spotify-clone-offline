d728ec838de053121ff4ba4fc0c06c270963e27c
AdnanSid795 <siddiquiadnan327@gmail.com>
2025-11-13 22:04:17 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,28 +1,33 @@
+const CACHE_NAME = "spotify-cache-v2";
+
+const FILES_TO_CACHE = [
+  "/Spotify-clone-offline/",
+  "/Spotify-clone-offline/index.html",
+  "/Spotify-clone-offline/manifest.json",
+  "/Spotify-clone-offline/icon-192.png",
+  "/Spotify-clone-offline/icon-512.png",
+
+  // MUSIC FILES
+  "/Spotify-clone-offline/music/song0.mp3",
+  "/Spotify-clone-offline/music/song1.mp3",
+  "/Spotify-clone-offline/music/song2.mp3",
+  "/Spotify-clone-offline/music/song3.mp3",
+  "/Spotify-clone-offline/music/song4.mp3",
+  "/Spotify-clone-offline/music/song5.mp3",
+  "/Spotify-clone-offline/music/song6.mp3",
+  "/Spotify-clone-offline/music/song7.mp3",
+  "/Spotify-clone-offline/music/song8.mp3",
+  "/Spotify-clone-offline/music/song9.mp3",
+  "/Spotify-clone-offline/music/song10.mp3"
+];
+
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open("spotify-cache-v1").then(cache => {
-      return cache.addAll([
-        "./",
-        "/index.html",
-        "/manifest.json",
-        "/icon-192.png",
-        "/icon-512.png",
-
-        // --- MUSIC FILES ---
-        "/music/song0.mp3",
-        "/music/song1.mp3",
-        "/music/song2.mp3",
-        "/music/song3.mp3",
-        "/music/song4.mp3",
-        "/music/song5.mp3",
-        "/music/song6.mp3",
-        "/music/song7.mp3",
-        "/music/song8.mp3",
-        "/music/song9.mp3",
-        "/music/song10.mp3"
-      ]);
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
     })
   );
+  self.skipWaiting();
 });
 
 self.addEventListener("fetch", event => {
@@ -31,4 +36,17 @@ self.addEventListener("fetch", event => {
       return response || fetch(event.request);
     })
   );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      )
+    )
+  );
+  self.clients.claim();
 });
