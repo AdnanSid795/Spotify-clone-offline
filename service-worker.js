3a0ab236397718c7cc697e050a56aa188b0dd026
AdnanSid795 <siddiquiadnan327@gmail.com>
2025-11-14 14:01:25 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,37 +1,46 @@
-const CACHE_NAME = "spotify-cache-v5";
+const CACHE_NAME = "spotify-cache-v6";
+const PREFIX = "/Spotify-clone-offline";
 
 const FILES_TO_CACHE = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./icon-192.png",
-  "./icon-512.png",
+  `${PREFIX}/`,
+  `${PREFIX}/index.html`,
+  `${PREFIX}/manifest.json`,
+  `${PREFIX}/icon-192.png`,
+  `${PREFIX}/icon-512.png`,
 
-  // MUSIC FILES
-  "./music/song1.mp3",
-  "./music/song2.mp3",
-  "./music/song3.mp3",
-  "./music/song4.mp3",
-  "./music/song5.mp3",
-  "./music/song6.mp3",
-  "./music/song7.mp3",
-  "./music/song8.mp3",
-  "./music/song9.mp3",
-  "./music/song10.mp3"
+  // MUSIC
+  `${PREFIX}/music/song1.mp3`,
+  `${PREFIX}/music/song2.mp3`,
+  `${PREFIX}/music/song3.mp3`,
+  `${PREFIX}/music/song4.mp3`,
+  `${PREFIX}/music/song5.mp3`,
+  `${PREFIX}/music/song6.mp3`,
+  `${PREFIX}/music/song7.mp3`,
+  `${PREFIX}/music/song8.mp3`,
+  `${PREFIX}/music/song9.mp3`,
+  `${PREFIX}/music/song10.mp3`
 ];
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
   );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => 
+      Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k)))
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then(response => 
+      response || fetch(event.request)
+    )
   );
 });
